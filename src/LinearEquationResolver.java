79dddd709a6f225e22e9a046500b67adbf516c50
tranthemanh <tranmanhhuha@gmail.com>
2024-06-06 14:50:26 +0700

    add LinearEquationResolver

---
+++ src/LinearEquationResolver.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class LinearEquationResolver {
+    public static void main(String[] args) {
+        System.out.println("Linear Equation Resolver");
+        System.out.println("Given a equation as 'a * x + b = c, please enter constants: ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("a: ");
+        double a = sc.nextDouble();
+        System.out.println("b: ");
+        double b = sc.nextDouble();
+        System.out.println("c: ");
+        double c = sc.nextDouble();
+
+        if (a != 0){
+            double answer = (c - b ) / a;
+            System.out.printf("Equation pass with x = %f!\n", answer);
+        } else {
+            if (b == c){
+                System.out.print("The solution is all x!");
+            }else {
+                System.out.print("No solution found!");
+            }
+        }
+
+    }
+}
