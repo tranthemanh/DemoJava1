6d0a52347c09f5b034f698a421c70c90b6775ad1
tranthemanh <tranmanhhuha@gmail.com>
2024-06-06 14:30:07 +0700

    add VariableDataType

---
+++ src/VariableDataType.java
@@ -0,0 +1,18 @@
+public class VariableDataType {
+    public static void main(String[] args) {
+        int a = 1;
+        float b = 1.1f;
+        double c = 1.2;
+        boolean d = true;
+        char c1 = 'a';
+        String d1 = "abc";
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("c1 = " + c1);
+        System.out.println("d1 = " + d1);
+        ;
+    }
+}
